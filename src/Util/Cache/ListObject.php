fe4eddfe7ae8b4069f4a482aa76ce8f1f0ef06e0
Jim Liu <hldh214@Gmail.com>
2019-01-02 10:50:46 +0800

    fix typo && add rpop lRem rpoplpush

--- src/Util/Cache/ListObject.php
+++ src/Util/Cache/ListObject.php
@@ -4,12 +4,44 @@ namespace Star\Util\Cache;
 
 /**
  * List 缓存结构
- *  - 基于字符串[List]
+ *  - 基于列表[List]
  *
  * @package Star\Util\Cache
  */
 Trait ListObject
 {
+    /**
+     * Pops a value from the tail of a list, and pushes it to the front of another list.
+     * Also return this value.
+     *
+     * @since   redis >= 1.1
+     * @param   string $srcKey
+     * @param   string $dstKey
+     * @return  string  The element that was moved in case of success, FALSE in case of failure.
+     * @link    https://redis.io/commands/rpoplpush
+     */
+    public function rpoplpush($srcKey, $dstKey)
+    {
+        return self::connect()->rpoplpush(self::$prefix . $srcKey, self::$prefix . $dstKey);
+    }
+
+    /**
+     * Removes the first count occurences of the value element from the list.
+     * If count is zero, all the matching elements are removed. If count is negative,
+     * elements are removed from tail to head.
+     *
+     * @param   string $key
+     * @param   string $value
+     * @param   int $count
+     * @return  int     the number of elements to remove
+     * bool FALSE if the value identified by key is not a list.
+     * @link    https://redis.io/commands/lrem
+     */
+    public function lRem($key, $value, $count)
+    {
+        return self::connect()->lRem(self::$prefix . $key, $value, $count);
+    }
+
     /**
      * rpush with bulk support
      * https://redis.io/commands/rpush
@@ -25,6 +57,18 @@ Trait ListObject
         }
 
         return self::connect()->rpush(self::$prefix . $key, $value);
+    }
+
+    /**
+     * rpop
+     * https://redis.io/commands/rpop
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function rpop($key)
+    {
+        return self::connect()->rpop(self::$prefix . $key);
     }
 
     /**
